e6e7ba5adfbe2656025d964c8e3931b9f54f861b
yasu1125 <v22.m1125@gmail.com>
2021-04-22 20:30:26 +0900

    initial commit

--- task12App/ViewController.swift
+++ task12App/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var taxExcludedField: UITextField!
+    @IBOutlet private weak var taxRateField: UITextField!
+    @IBOutlet private weak var taxIncludedLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        if let text = UserDefaults.standard.string(forKey: "text") {
+            self.taxRateField.text = text
+        }
     }
 
+    private func taxRateCalculator(n: Double) -> Double {
 
+        var tax: Double
+
+        tax = n / 100 + 1
+
+        return tax
+    }
+
+    @IBAction func didTapButton(_ sender: Any) {
+
+        let taxExcludedPrice = Int(taxExcludedField.text!) ?? 0
+        let taxRate = taxRateCalculator(n: Double(taxRateField.text!) ?? 0)
+
+        let result = Int(Double(taxExcludedPrice) * taxRate)
+
+        taxIncludedLabel.text = "\(result)"
+
+        UserDefaults.standard.set(self.taxRateField.text, forKey: "text")
+    }
 }
 
